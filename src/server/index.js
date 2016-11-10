6ad266d898d798cb21bdc5980ef771b23951bc80
jmichelin <jmichelinphone@gmail.com>
2016-11-10 14:42:11 -0500

    ci(travis): add code coverage

---
+++ src/server/index.js
@@ -0,0 +1,10 @@
+'use strict';
+
+var gimme1 = function gimme1(){
+  return 1;
+};
+
+module.exports = {
+  gimme1: gimme1()
+};
+
--- src/test/server/index.test.js
+++ src/test/server/index.test.js
@@ -1,7 +1,12 @@
 var expect = require('chai').expect;
+var myApi = require('../../server/index.js');
 
 describe('Our first tests', function () {
   it('expect framework should be installed', function() {
     expect(expect).to.exist;
   });
+
+  it('expect gimme1 to return 1', function() {
+    expect(myApi.gimme1).to.be.equal(1);
+  })
 });
